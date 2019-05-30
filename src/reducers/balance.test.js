48c4183478ec0ab10037d6d989a1a0ea74926987
Alex Cho <alexsjcho@gmail.com>
2019-05-29 19:06:55 -0700

    added Redux Deposit, Withdraw methods and test

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -9,4 +9,21 @@ describe('balanceReducer', () => {
       balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
     ).toEqual(balance);
   });
+
+  it('deposits into the balance', () => {
+    const deposit = 10;
+    const initialState = 5;
+    expect(
+      balanceReducer(initialState, { type: constants.DEPOSIT, deposit })
+    ).toEqual(initialState + deposit);
+  });
+
+  it('withdraws from the balance', () => {
+    const withdrawal = 10;
+    const initialState = 20;
+
+    expect(
+      balanceReducer(initialState, { type: constants.WITHDRAW, withdrawal })
+    ).toEqual(initialState - withdrawal);
+  });
 });
