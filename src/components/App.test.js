c1f12944273e409a3ed158412936ce84bb26a7e8
Alex Cho <alexsjcho@gmail.com>
2019-05-29 18:46:40 -0700

    rendered Wallet component, plus passed Wallet tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -11,4 +11,7 @@ describe('App', () => {
   it('renders properly', () => {
     expect(app).toMatchSnapshot();
   });
+  it('contains a connected Wallet component', () => {
+    expect(app.find('Connect(Wallet)').exists()).toBe(true);
+  });
 });
--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -11,9 +11,11 @@ export class Wallet extends Component {
   }
 }
 
+const mapStateToProps = (state) => ({
+  balance: state
+});
+
 export default connect(
-  (state) => {
-    balance: state;
-  },
+  mapStateToProps,
   null
 )(Wallet);
